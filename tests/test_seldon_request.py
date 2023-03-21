cdd52639cd03300406859f5ae625a0f296ca49aa
Trevor Royer <troyer@redhat.com>
2023-03-21 09:36:03 -0500

    cleanup old files

--- tests/test_seldon_request.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import os
-from iris_model_training.seldon_request import get_iris_species
-
-def test_get_iris_species():
-    assert get_iris_species(0) == "Iris-setosa"
-    assert get_iris_species(1) == "Iris-versicolor"
-    assert get_iris_species(2) == "Iris-virginica"
--- tests/test_train.py
+++
@@ -1,5 +0,0 @@
-from iris_model_training.train import my_function
-
-
-def test_my_function():
-    assert my_function(3) == 4
